865d170ec96c447d36ec0564707bd2e83d20835d
UwayoOlga <uwayoolga@gmail.com>
2025-06-29 08:10:26 +0200

    added Front end

---
+++ src/main/java/rw/ac/auca/ecommerce/controller/HomeController.java
@@ -0,0 +1,18 @@
+package rw.ac.auca.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
+    }
+} 
